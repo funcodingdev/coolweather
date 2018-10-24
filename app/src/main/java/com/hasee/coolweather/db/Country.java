0dd8495462ae897a4816b8cc9c1c66ebb0784e77
1342526004 <1342526004@qq.com>
2018-10-24 21:51:49 +0800

    加入创建数据库和表的各项设置

---
+++ app/src/main/java/com/hasee/coolweather/db/Country.java
@@ -0,0 +1,42 @@
+package com.hasee.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;//县id
+    private String countryName;//县名
+    private String weatherId;//该县的天气Id
+    private int cityId;//所属市的id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/hasee/coolweather/db/Province.java
@@ -0,0 +1,33 @@
+package com.hasee.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;//省份id
+    private String provinceName;//省名
+    private String provinceCode;//省代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
